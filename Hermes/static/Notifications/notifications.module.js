57f03f33b4d2eb821cdcfdfd22f5c7ad8fe10cb9
Laurent Baeriswyl <lbaerisw@cisco.com>
2017-04-27 09:20:34 -0700

    Added Notifications module config & deleted testDash

--- Hermes/static/Notifications/notifications.module.js
+++ Hermes/static/Notifications/notifications.module.js
@@ -3,6 +3,19 @@
   'use strict';
 
   angular
-    .module('Notifications', []);
+    .module('Notifications', [])
+    .config(config);
+
+  config.$inject = ['$provide', '$windowProvider', '$routeProvider'];
+
+  function config($provide, $windowProvider, $routeProvider) {
+    var path = $windowProvider.$get().STATIC_URL + 'Notifications/';
+    $provide.constant('horizon.dashboard.Notifications.basePath', path);
+
+    $routeProvider.when('/Notifications', {
+      templateUrl: path + 'table.html'
+    });
+
+  }
 
 })();
