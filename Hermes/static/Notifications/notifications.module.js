57549759424115eac3c47055cb04af8b2c1ea4c4
Laurent Baeriswyl <lbaerisw@cisco.com>
2017-04-27 09:25:52 -0700

    Fixed module path to include admin

--- Hermes/static/Notifications/notifications.module.js
+++ Hermes/static/Notifications/notifications.module.js
@@ -9,8 +9,8 @@
   config.$inject = ['$provide', '$windowProvider', '$routeProvider'];
 
   function config($provide, $windowProvider, $routeProvider) {
-    var path = $windowProvider.$get().STATIC_URL + 'Notifications/';
-    $provide.constant('horizon.dashboard.Notifications.basePath', path);
+    var path = $windowProvider.$get().STATIC_URL + 'dashboard/admin/Notifications/';
+    $provide.constant('horizon.dashboard.admin.Notifications.basePath', path);
 
     $routeProvider.when('/Notifications', {
       templateUrl: path + 'table.html'
